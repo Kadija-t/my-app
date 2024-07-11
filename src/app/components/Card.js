63dfc087b2d191ef1c38490c38aa4be922110e17
Kadija T <hmani.kadija11@gmail.com>
2024-07-11 11:30:14 +0200

    layout cards + redirect to houses page onclick

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -11,15 +11,34 @@ style sass => A faire
 
   import React from 'react'
   import House from '../data/logements.json'
+ //import { Link } from 'react-router-dom'
+  import { useNavigate } from 'react-router-dom'
   
-  function Card() {
+
+    //state
+ //   let [house, setHouse] =  useState (false) ;
+
+    //comportements
+   
+    // function handleClick(useState) {
+    //     if (useState === true){
+    //         <Link></Link>
+    //     }
+    //     alert(`Voulez-vous allez sur la page du logement: ${title}`)
+    // }
+    //reder
+
+  const Card = () => { 
+    const navigate = useNavigate();
     return (
       <>
-    <div className="Info">
-        {
-            House.map (house => {
+        <div className="Info">
+          {House.map (house => {
                 return(
-                    <div className="box" key={ house.id }>
+                    <div className="box" key={ house.id} onClick={() => {navigate ("/houses");}}
+                     style = {{
+                        backgroundImage: `url(${house.cover})`
+                    } }>
                         <h3>{ house.title }</h3><br></br>
                         <img src={ house.cover } alt =""/>
                     </div>
